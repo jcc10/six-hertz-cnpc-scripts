566e2ad9515e4a7eafaadf263367adadf180e94d
Joshua Cline <wwjdtd@gmail.com>
2021-05-04 16:09:03 -0700

    Items have stubs.

---
+++ noppes.npcs.api/item/ItemBlock.lib.d.ts
@@ -0,0 +1,7 @@
+/// <reference no-default-lib="true"/>
+/// <reference path="./ItemStack.lib.d.ts"/>
+
+//deno-lint-ignore no-empty-interface
+interface ItemBlock extends ItemStack {
+    //TODO: Stub
+}
---
+++ noppes.npcs.api/item/ItemBook.lib.d.ts
@@ -0,0 +1,7 @@
+/// <reference no-default-lib="true"/>
+/// <reference path="./ItemStack.lib.d.ts"/>
+
+//deno-lint-ignore no-empty-interface
+interface ItemBook extends ItemStack {
+    //TODO: Stub
+}
